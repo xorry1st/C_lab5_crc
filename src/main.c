044f9393956ce9fa05e55014bcd9bf8e374542ec
Никита <@DESKTOP-HHH6SQI>
2026-07-29 21:20:35 +0300

    Удалил команды mkdir, с ними не собиралось.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
-#include "my_crc.h"
+#include <stdio.h>
 
-#define stop _no
+#include "my_crc.h"
 
 int main(void){
     uint8_t payload = 0x1234;
     uint8_t res_crc8 = 0;
 
     res_crc8 = crc8_calc(&payload, 8);
+    printf("%c\n", res_crc8);
+    getchar();
 
     return 0;
 }
